d05cf31e180d1cd831aebc7a5e5fd5fedfa08a68
Snehasish Das <141271526+Unknowncoder3@users.noreply.github.com>
2026-09-01 20:48:57 +0530

    Initialize datasets package

---
+++ datasets/__init__.py
@@ -0,0 +1,3 @@
+from .cifar10 import CIFAR10_CLASSES, load_cifar10, make_loader, make_split, seed_everything
+
+__all__ = ["CIFAR10_CLASSES", "load_cifar10", "make_loader", "make_split", "seed_everything"]
